729b93224be04417f73653805dd2e05a321be1fa
yoongyum <yoongyum96@telcoware.com>
2023-03-29 21:41:21 +0900

    feat: Modify WebSockConfig
    메시지 브로커는 일단 사용안함.
    일단 기본 configurer 로 구현

--- src/main/java/com/denimdingo/tikitaka/configuration/WebSockConfig.java
+++ src/main/java/com/denimdingo/tikitaka/configuration/WebSockConfig.java
@@ -1,35 +1,21 @@
 package com.denimdingo.tikitaka.configuration;
 
+import com.denimdingo.tikitaka.handler.WebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
-@EnableWebSocketMessageBroker
+@EnableWebSocket
 @Configuration
-public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
-
+public class WebSockConfig implements WebSocketConfigurer {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(signalingSocketHandler(), "/room")
+                .setAllowedOrigins("*");
     }
 
     @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer()
-    {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxBinaryMessageBufferSize(10000);
-        container.setMaxSessionIdleTimeout(1000L);
-        container.setAsyncSendTimeout(1000L);
-        return container;
+    public WebSocketHandler signalingSocketHandler() {
+        return new WebSocketHandler();
     }
 }
