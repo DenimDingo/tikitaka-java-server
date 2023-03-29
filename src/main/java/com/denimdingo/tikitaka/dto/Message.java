752e15b86f5cb4dcdc493531da1ae191985422e9
yoongyum <yoongyum96@telcoware.com>
2023-03-29 21:40:24 +0900

    feat: Create Message

--- src/main/java/com/denimdingo/tikitaka/dto/Message.java
+++ src/main/java/com/denimdingo/tikitaka/dto/Message.java
@@ -1,9 +1,29 @@
 package com.denimdingo.tikitaka.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
-    private String from;
-    private String text;
+    private String type;
+    private String sender;
+    private String receiver;
+    private String data;
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void newConnect() {
+        this.type = "new";
+    }
+
+    public void closeConnect() {
+        this.type = "close";
+    }
 }
