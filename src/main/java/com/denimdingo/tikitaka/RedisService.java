2dfeb57470a34d39304e5f47e1d1dbcbff2c7c48
Epsilonely <yoongyum@kakao.com>
2023-05-01 19:17:08 +0900

    feat: Modify Set data in redis
    redis 에 데이터 저장되는 것은 확인했으나, 객체 자체를 저장하는 것을 구현해야함.

--- src/main/java/com/denimdingo/tikitaka/RedisService.java
+++ src/main/java/com/denimdingo/tikitaka/RedisService.java
@@ -1,10 +1,12 @@
 package com.denimdingo.tikitaka;
 
+import com.denimdingo.tikitaka.dto.AuthReq;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -15,24 +17,27 @@ public class RedisService {
     private static final Logger redisLogger = LoggerFactory.getLogger(RedisService.class);
 
     /**
-     * Get data value in Redis database
-     * @param key data 키
+     * Get data value in Redis
+     *
+     * @param key data
      */
     public void getRedisStringValue(String key) {
         ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
         redisLogger.info("Redis key   : {}", key);
         redisLogger.info("Redis value : {}", stringValueOperations.get(key));
+        // TODO:  예외처리
     }
 
     /**
      * Set data in Redis
-     * @param key data 키
-     * @param value data 값
+     *
+     * @param data data 키
      */
-    public void setRedisStringValue(String key, String value) {
+    public void setRedisStringValue(AuthReq.SignUp data) {
         ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
-        stringValueOperations.set(key, value);
-        redisLogger.info("Redis key   : {}", key);
-        redisLogger.info("Redis value : {}", stringValueOperations.get(key));
+        new Jackson2JsonRedisSerializer<>(AuthReq.SignUp.class);
+        stringValueOperations.set(data.getId(), );
+        redisLogger.info("Redis key   : {}", data.getId());
+        redisLogger.info("Redis value : {}", stringValueOperations.get(data.getId()));
     }
 }
